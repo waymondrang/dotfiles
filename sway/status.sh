561aff1520f563ffb94acc5ca36ab87d6db0db1f
raymond <raymond@archbook.local>
2026-03-23 20:48:44 -0700

    update sway config files

--- sway/status.sh
+++ sway/status.sh
@@ -1,18 +1,29 @@
 #! /bin/sh
 
+# print header
 echo "{\"version\": 1}"
 echo "["
-echo "[]"
 
 while true; do
+    # get battery level
     battery_level=$(cat /sys/class/power_supply/*/capacity | head -n 1)
+
+    # charging indicator prefix
     charging_indicator=""
 
     if cat /sys/class/power_supply/*/status | grep -q -i "\bcharging"; then
         charging_indicator="*"
     fi
 
-    printf ',[{"name": "battery", "full_text": "%s%s"}]\n' "$battery_level" "$charging_indicator"
+    # header bracket
+    printf '['
+    
+    # body
+    printf '{"name": "battery", "full_text": "%s%s", "separator_block_width": 12}' "$battery_level" "$charging_indicator"
+    printf ',{"name": "time", "full_text": "%s", "separator_block_width": 12}' "$(date "+%m/%d/%Y %H:%M:%S")"
+    
+    # trailing bracket
+    printf '],\n'
 
     sleep 1
 done
